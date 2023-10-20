f8b659ff3860a4841268464bde7e586147deb7ba
Franziska Dreier <franzi@Franziskas-MacBook-Air.local>
2023-10-20 15:59:21 +0300

    Cleanup

--- functions_and_variables.py
+++ functions_and_variables.py
@@ -1,19 +1,25 @@
 ################ Global variables ################
 # paths for the three files
-train_file = "./data/train.txt"
-test_file = "./data/test.txt"
-val_file = "./data/val.txt"
-
-
-
-
+data_path = "./data/"
+result_path = "./results/"
+train_file = data_path + "train.txt"
+test_file = data_path + "test.txt"
+val_file = data_path + "val.txt"
+corpus = data_path + "corpus/"
 
 ################ Useful functions ################
 
 # process text files so that a list is returned with [<sentence>,<emotion>]
-def read_file(file_path):
+def read_file(file_path: str, delim: str):
     data = []
     with open(file_path,'r') as file:
         for line in file:
-            data.append(line.strip().split(";"))
+            data.append(line.strip().split(delim))
     return data
+
+# write text to file
+def write_to_file(file_path: str, content: str, new: bool):
+    if new: file = open(file_path,'w')
+    else: file = open(file_path,'a')
+    file.write(content)
+    file.close()
